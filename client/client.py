f0586a692dd5094a004d09cf6e6176854dbd63fe
Alex Reed <alexreed1192@gmail.com>
2020-07-19 09:43:25 -0700

    Add more sample files

---
+++ client/client.py
@@ -0,0 +1,12 @@
+
+from typing import List
+from typing import Dict
+from typing import Union
+
+class MyNewClass():
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
