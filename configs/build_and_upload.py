a49c341dabaa215bb5f3fb785f2e721772d93c5b
Alex Reed <alexreed1192@gmail.com>
2020-08-28 12:13:32 -0700

    more upload stuff

--- configs/build_and_upload.py
+++ configs/build_and_upload.py
@@ -1,6 +1,8 @@
 import pathlib
 import subprocess
 
+JUST_BUILD = True
+
 # Grab the current working directory.
 current_dir = pathlib.Path().cwd().as_posix()
 
@@ -12,18 +14,21 @@ build_result = subprocess.Popen(
 # Wait till we have the results.
 build_result.wait()
 
-# Upload to TestPyPi.
-upload_test_result = subprocess.Popen(
-    'twine upload -r testpypi dist/*', cwd=current_dir
-)
+# Only uploaded if needed.
+if JUST_BUILD == False:
 
-# Wait till we have the results.
-upload_test_result.wait()
+    # Upload to TestPyPi.
+    upload_test_result = subprocess.Popen(
+        'twine upload -r testpypi dist/*', cwd=current_dir
+    )
 
-# Upload to TestPyPi.
-upload_result = subprocess.Popen(
-    'twine upload dist/*', cwd=current_dir
-)
+    # Wait till we have the results.
+    upload_test_result.wait()
 
-# Wait till we have the results.
-upload_result.wait()
+    # Upload to TestPyPi.
+    upload_result = subprocess.Popen(
+        'twine upload dist/*', cwd=current_dir
+    )
+
+    # Wait till we have the results.
+    upload_result.wait()
